647464732ccf1ef18be7919f54e6eb01091af699
Rohit <srohit1101@gmail.com>
2020-05-14 22:07:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function Fancy(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"FancyBorder FancyBorder-" + props.color}>
+      {props.children}
     </div>
   );
 }
 
-export default App;
+export function App() {
+  return (
+    <div className="App">
+      <Fancy color="blue">
+        <h1>Welcome</h1>
+        <p>Thank you visiting our spacecraft</p>
+      </Fancy>
+    </div>
+  );
+}
